191b30a76ffb9298a071ce4537816099672d3f3d
nhan nguyen <nhan nguyen@DESKTOP-QRDST9N>
2020-10-27 00:04:50 +0700

    Add project files.

---
+++ ITravelService.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TravelServiceApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITravelService" in both code and config file together.
+    [ServiceContract]
+    public interface ITravelService
+    {
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+           ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+           UriTemplate = "api/v1/GetLocationList"
+         )] // định nghĩa đường link trả về
+        List<Location> GetLocation();
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+            UriTemplate = "api/v1/CreateLocation"
+          )] // định nghĩa đường link trả về
+
+        bool AddLocation(Location location);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "PUT",
+           ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+           UriTemplate = "api/v1/EditLocation/{id}"
+         )] // định nghĩa đường link trả về
+        bool EditLocation(string id, Location location);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "DELETE",
+           ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+           UriTemplate = "api/v1/DeleteLocation/{id}"
+         )] // định nghĩa đường link trả về
+        bool DeleteLocation(string id);
+
+        // POST Services
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+           ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+           UriTemplate = "api/v1/GetPostList"
+         )] // định nghĩa đường link trả về
+        List<Post> GetPosts();
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+           UriTemplate = "api/v1/CreatePost"
+         )] // định nghĩa đường link trả về
+        bool AddPost(Post post);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "PUT",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/EditPost/{id}"
+        )] // định nghĩa đường link trả về
+        bool EditPost(string id, Post post);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "POST",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/DeletePost/{id}"
+        )] // định nghĩa đường link trả về
+        bool DeletePost(string id);
+
+        // Comment Services
+
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/GetCommentList"
+        )] // định nghĩa đường link trả về
+        List<Comment> GetComments();
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+           UriTemplate = "api/v1/CreateComment"
+         )] // định nghĩa đường link trả về
+        bool AddComment(Comment comment);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "PUT",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/EditComment/{id}"
+        )] // định nghĩa đường link trả về
+        bool EditComment(string id, Comment comment);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "POST",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/DeleteComment/{id}"
+        )] // định nghĩa đường link trả về
+        bool DeleteComment(string id);
+
+
+        // Images Services
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/GetImageList"
+        )] // định nghĩa đường link trả về
+        List<Image> GetImages();
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "POST",
+           ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+           RequestFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+           UriTemplate = "api/v1/CreateImage"
+         )] // định nghĩa đường link trả về
+        bool AddImage(Image image);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "PUT",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/EditImage/{id}"
+        )] // định nghĩa đường link trả về
+        bool EditImage(string id, Image image);
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "POST",
+          ResponseFormat = WebMessageFormat.Json, // format kiểu trả về và gửi lên là json
+          RequestFormat = WebMessageFormat.Json,
+          BodyStyle = WebMessageBodyStyle.Bare, // style dữ liệu trả về
+          UriTemplate = "api/v1/DeleteImage/{id}"
+        )] // định nghĩa đường link trả về
+        bool DeleteImage(string id);
+    }
+}
---
+++ TravelService.svc.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace TravelServiceApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TravelService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TravelService.svc or TravelService.svc.cs at the Solution Explorer and start debugging.
+    public class TravelService : ITravelService
+    {
+        DataClasses1DataContext data = new DataClasses1DataContext();
+        public bool AddLocation(Location location)
+        {
+
+            try
+
+            {
+                data.Locations.InsertOnSubmit(location);
+                data.SubmitChanges();
+                
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteLocation(string id)
+        {
+
+            try
+            {
+                var location = data.Locations.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                data.Locations.DeleteOnSubmit(location);
+                data.SubmitChanges();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        public void DoWork()
+        {
+        }
+
+        public bool EditLocation(string id, Location newlocation)
+        {
+            try
+            {
+                var location = data.Locations.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                location.id = newlocation.id;
+                location.LocationName = newlocation.LocationName;
+                location.LocationAddress = newlocation.LocationAddress;
+                location.LocationDescription = newlocation.LocationDescription;
+                location.Status = newlocation.Status;
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Location> GetLocation()
+        {
+            try
+            {
+                var locations = (from location in data.Locations select location).ToList(); 
+
+                return locations;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public bool AddComment(Comment comment)
+        {
+            try
+            {
+                data.Comments.InsertOnSubmit(comment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool AddImage(Image image)
+        {
+            try
+            {
+                data.Images.InsertOnSubmit(image);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool AddPost(Post post)
+        {
+            try
+            {
+                data.Posts.InsertOnSubmit(post);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool DeleteComment(string id)
+        {
+            try
+            {
+                var comment = data.Comments.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                data.Comments.DeleteOnSubmit(comment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool DeleteImage(string id)
+        {
+            try
+            {
+                var image = data.Images.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                data.Images.DeleteOnSubmit(image);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool DeletePost(string id)
+        {
+            try
+            {
+                var post = data.Posts.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                data.Posts.DeleteOnSubmit(post);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditComment(string id, Comment newComment)
+        {
+            try
+            {
+                var comment = data.Comments.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                data.Comments.DeleteOnSubmit(comment);
+                data.Comments.InsertOnSubmit(newComment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditImage(string id, Image newImage)
+        {
+            try
+            {
+                var image = data.Images.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                data.Images.DeleteOnSubmit(image);
+                data.Images.InsertOnSubmit(newImage);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool EditPost(string id, Post newPost)
+        {
+            try
+            {
+                var post = data.Posts.Where(b => b.id == int.Parse(id)).FirstOrDefault();
+                data.Posts.DeleteOnSubmit(post);
+                data.Posts.InsertOnSubmit(newPost);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+        public List<Comment> GetComments()
+        {
+            try
+            {
+                var comments = (from comment in data.Comments select comment).ToList();
+                return comments;
+            }
+            catch { return null; }
+        }
+
+        public List<Image> GetImages()
+        {
+            try
+            {
+                var images = (from image in data.Images select image).ToList();
+                return images;
+            }
+            catch { return null; }
+        }
+        public List<Post> GetPosts()
+        {
+            try
+            {
+                var posts = (from post in data.Posts select post).ToList();
+                return posts;
+            }
+            catch { return null; }
+        }
+    }
+}
